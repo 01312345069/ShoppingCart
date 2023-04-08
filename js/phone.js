bfb44192d5d4e2529b8955f6cde354b29b93a3c8
Md Naymul Islam মোঃ নয়িমুল ইসলাম <mybspi@gmail.com>
2023-04-08 22:24:18 +0600

    Calculate subtotal tax amount and final total

--- js/phone.js
+++ js/phone.js
@@ -31,16 +31,21 @@ function updatePhoneTotalPrice(naymul) {
     phoneTotalElement.innerText = phoneTotalPrice;
 }
 
+
+
+
 document.getElementById('btn-phone-plus').addEventListener('click', function () {
     const newPhoneNumber = updatePhoneNumber(true);
 
     updatePhoneTotalPrice(newPhoneNumber);
+    calculateSubTotal();
 
-})
+});
 document.getElementById('btn-phone-minus').addEventListener('click', function () {
     const newPhoneNumber = updatePhoneNumber(false);
 
     updatePhoneTotalPrice(newPhoneNumber);
+    calculateSubTotal();
 })
 
 
